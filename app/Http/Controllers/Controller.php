ce0812b308bc01a5fd1871f11d74780e4d110e56
ItzKazuki <ibnu235729@gmail.com>
2024-08-23 09:16:25 +0700

    create 3 table, and views

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+
 abstract class Controller
 {
-    //
+    // private function checkIsNumber($data)
+    // {
+    //     return
+    // }
+
+    public function showError(Exception $e, string $message, int $code) {
+        return response()->json([
+            'message' => $message ?? "",
+            'error' => $e->getMessage(),
+            'code' => $code
+        ], $code);
+    }
+
+    public function showSuccess(string $message, $code = 200) {
+        return response()->json([
+            'message' => $message,
+            'code' => $code
+        ], $code);
+    }
 }
---
+++ database/migrations/2024_08_22_234823_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('seller_id');
+            $table->foreign('seller_id')->on('users')->references('id');
+            $table->string('nama');
+            $table->decimal('harga');
+            $table->string('desc');
+            $table->integer('stok');
+            $table->string('nama_penjual');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
